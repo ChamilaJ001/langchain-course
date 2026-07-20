5ae1a5082de8a8502240875ab667121f9c84314c
Chamila Jayasinghe <official.chamilaj@gmail.com>
2026-07-20 10:05:35 +0530

    document loaders

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+from dotenv import load_dotenv
+from langchain_core import __version__ as core_version
+from langchain_openai import ChatOpenAI
+
+print(f"lang version: {core_version}")
+
+load_dotenv()
+
 def main():
-    print("Hello from langchain-course!")
+    llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+    response = llm.invoke("Say 'setup complete' in one word")
+    print(f"Response from LLM: {response}")
 
 
 if __name__ == "__main__":
